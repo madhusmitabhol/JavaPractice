68aea87261a9a586ec8f77705824c700677ee498
Madhusmita <bholmadhusmita@gmail.com>
2017-05-29 16:44:43 +0530

    com/mstutorial02/Application04.java

---
+++ src/com/mstutorial02/Application04.java
@@ -0,0 +1,23 @@
+package com.mstutorial02;
+
+public class Application04 {
+
+	public static void main(String[] args) {
+		int a=70;
+		float b=12.6f;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("**************");
+		 a= 80;
+		 b=90.8f;
+		 
+		 System.out.println(a);
+		 System.out.println(b);
+		 
+		
+		
+		
+
+	}
+
+}
--- src/com/mstutorial02/Readme.txt
+++ src/com/mstutorial02/Readme.txt
@@ -3,3 +3,4 @@ Finished: Video02 :29-May 15:35
 "Using Variables"
 1st program:numbers
 2nd program: copying a value from one variable to other.
+3rd Program: Variable Re initialization
